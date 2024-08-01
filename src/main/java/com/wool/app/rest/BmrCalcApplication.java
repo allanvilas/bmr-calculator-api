d9041acee29fc17211d27b70aefa187854e2394e
allanvilas <allanvilasdeveloper@gmail.com>
2024-07-31 22:50:46 -0300

    feat(base-project): Add basic project exported from maven initalizr - Add the project base files

---
+++ src/main/java/com/wool/app/rest/BmrCalcApplication.java
@@ -0,0 +1,13 @@
+package com.wool.app.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmrCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmrCalcApplication.class, args);
+	}
+
+}
