ebe88a2cfc70ec3802265b1753aed3a334f48141
allanvilas <allanvilasdeveloper@gmail.com>
2024-08-01 23:54:19 -0300

    add(baseProject):

---
+++ src/test/java/com/bmr/calc/BmrCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmr.calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmrCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
